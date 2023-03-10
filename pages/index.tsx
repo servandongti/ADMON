1175b4336f83f5376de86fc435772d0de0b9e469
yoGhastly <ve06976@innovaccion.mx>
2023-03-09 23:23:12 -0600

    framer motion and supabase libraries

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,84 +1,57 @@
-import type { NextPage } from 'next'
+import type { GetServerSideProps, InferGetServerSidePropsType, NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
+import { Cube } from '../components/Cube'
+
 
 const Home: NextPage = () => {
+
+  const [hasMounted, setHasMounted] = useState(false)
+  const router = useRouter()
+  useEffect(() => (
+    setHasMounted(true)
+  ), [])
+
+  if (!hasMounted) {
+    return null
+  }
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>Juego Clasificador de Formas</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <div className='flex justify-center items-center z-50 absolute bg-black w-full h-full' style={{ backgroundColor: 'rgba(55,55,55,0.5)' }}>
+        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full' onClick={() => router.push('/register')}>
+          Jugar
+        </button>
+      </div>
+      <nav className="z-10 flex flex-row justify-between w-full max-w-4xl px-4 py-6">
+        <div className='w-32 h-20 bg-slate-400 text-white flex justify-center items-center rounded-md'>
+          Puntuación: {0}
         </div>
+
+        <div className='w-32 h-20 bg-slate-400 text-white flex justify-center items-center rounded-md'>
+          Intentos: {0}
+        </div>
+      </nav>
+
+      <main className="z-10 flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
+        <Cube />
       </main>
 
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
+      <footer className='z-10 flex gap-5 h-24 w-full max-w-5xl items-center justify-center border border-black rounded-xl'>
+        {
+          ["/square.svg", "/circle.svg", "/triangle.svg", "/star.svg"].map((image, index) => (
+            <div className='w-20 h-20 bg-slate-400 text-white flex gap-5 justify-center items-center rounded-md'>
+              <Image src={image} key={index} alt={`image-${index}`} width={40} height={40} />
+            </div>
+          ))
+        }
       </footer>
+
     </div>
   )
 }
